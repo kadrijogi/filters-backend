b84f410b568ebca08d292790fe6146a8d82a62a3
kadri.jogi <kadri.jogi@helmes.com>
2024-11-09 18:53:04 +0200

    WebConfig

---
+++ src/main/java/com/filters/demo/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.filters.demo.config;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://kadrijogi.github.io/filters-app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow specific HTTP methods
+                .allowedHeaders("*")  // Allow any header
+                .allowCredentials(true);  // Allow cookies if needed
+    }
+   
+}
